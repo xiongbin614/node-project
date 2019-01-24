b8c9c876a336db7bdb3553ac988e0dd1a55e2dc4
xiongbin <1490251116@qq.com>
2019-01-24 17:41:33 +0800

    banner完成差不多，差添加、删除图片、

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,111 +1,257 @@
 
-$(function(){
-
-    clickPage();
-    delet();
-    alter();
-    addBanner();
-    // $('.alter')[0].click(function(){
-    //     alert(5)
-    // })
-    // getData(pageNum, pageSize)第一个多少页，第二个是一页有多少条
+var result = [];
+var totalPage = 0;
+var page=1;
+var pageNum=1;
+var pageSize=2;
+$(function () {
     // 添加banner数据
-    function addBanner(){
-        $("#bannerAdd").click(function () {
-            // 发送ajax请求数据到后台
-            $.post('/banner/add', {
-                vdName: $('#inputEmail3').val(),
-                imgName: $('#inputPassword3').val()
+    addBanner();
+    //点击删除banner数据
+    delet();
+    //将banner数据渲染在页面上
+    getData(pageNum, pageSize);
+    // getData(1, 4);
+    //点击切换页面
+    clickPage();
+
+    prevPage();
+});
+
+// 添加banner数据
+function addBanner(name, ingUrl) {
+    $("#bannerAdd").click(function () {
+        // 1. 实例化一个 FormData 对象
+        var formData = new FormData();
+        // 2. 给 formData 对象 加属性
+        formData.append('bannerName', $('#inputEmail3').val());
+        formData.append('bannerImg', $('#inputPassword3')[0].files[0]);
+        console.log(pageNum, pageSize);
+        // 发送ajax请求数据到后台
+        $.ajax({
+            url: '/banner/add',
+            method: 'POST',
+            contentType: false,
+            processData: false,
+            data: formData,
+            success: function () {
+            layer.msg('添加成功');
+            $('#myModal').modal('hide');
+            $('#inputEmail3').val('');
+            $('#inputPassword3').val('');
+            getData(page, pageSize);
+            },
+            error: function (error) {
+                console.log(error.message);
+                layer.msg('网络异常，请稍后重试');
+            },
+        })
+    });
+}
+// function addBanner(name, ingUrl) {
+//     $("#bannerAdd").click(function () {
+//         getData(pageNum, pageSize);
+//         // getData(1, 3);
+//         // 发送ajax请求数据到后台
+//         $.post('/banner/add', {
+//             vdName: $('#inputEmail3').val(),
+//             imgName: $('#inputPassword3').val()
+//         }, function (res) {
+//             if (res.code === 0) {
+//                 // 成功
+//                 layer.msg('提交成功');
+//             } else {
+//                 // 失败
+//                 alert("网络异常，请稍后再试！！")
+//             }
+//             $('#myModal').modal('hide');
+//             $('#inputEmail3').val('');
+//             $('#inputPassword3').val('');
+//         })
+//     });
+// }
+//删除一条banner数据
+function delet() {
+    $(".xb-show tbody").delegate(".delet", "click", function (event) {
+        layer.confirm('是否要删除?', function (index) {
+            var target = $(event.target);
+            // alert(target.data('id'));
+            target.parent().parent().remove();
+            $.get('/banner/remove', {
+                name: target.data('id')
             }, function (res) {
                 if (res.code === 0) {
-                    // 成功
-                    layer.msg('提交成功');
-                    console.log('提交成功');
+                    layer.msg('删除成功');
+                    //删除public下面的图片文件
+                   console.log($(".xb-show tbody").html())
+                    // if ($(".xb-show tbody").html()){
+                    //     console.log(555)
+                    //     page--;
+                    //     getData(page, pageSize);
+                    // }else{
+                    //     console.log(6666)
+                    // }
+                    // if (totalPage - page==1){
+                    //     page--;
+                    // }
+                    // console.log('sss'+totalPage)
+                    getData(page, pageSize);
+                    // load();
                 } else {
-                    // 失败
-                    console.log(res.message);
-                    alert("网络异常，请稍后再试！！")
+                    layer.msg('网络异常，请稍后再试');
                 }
-                $('#myModal').modal('hide');
-                $('#inputEmail3').val('');
-                $('#inputPassword3').val('');
             })
+            layer.close(index);
         });
-    }
-    
-});
-// 发送ajax渲染页面
-function getData(pageNum, pageSize){
-    $.get('/banner/get',{
+    })
+}
+// 发送ajax获取数据
+function getData(pageNum, pageSize) {
+    $.get('/banner/get', {
         pageNum: pageNum,
         pageSize: pageSize
     },
-    function(res){
-        if (res.code===0){//  chengghuoqu
-            for (var i = 0; i < res.data.length;i++){
-               $('.xb-show tbody').append(
+        function (res) {
+            if (res.code === 0) {
+                totalPage = res.totalPage;//页数
+                result = res.data;//banner数据
+                // console.log(result.length);
+                showTable(result);
+                showPage(totalPage);
+            } else {//获取失败
+                layer.msg('网络异常，请稍后重试');
+            }
+        })
+};
+function load(){
+    $.get('/banner/get', {
+        pageNum: pageNum,
+        pageSize: pageSize
+    },
+        function (res) {
+            if (res.code === 0) {
+                totalPage = res.totalPage;//页数
+                result = res.data;//banner数据
+                // console.log(result.length);
+                showTable(result);
+                showPage(totalPage);
+            } else {//获取失败
+                layer.msg('网络异常，请稍后重试');
+            }
+        })
+}
+//渲染表格
+function showTable(result) {
+    $('.xb-show tbody').html('');
+    for (var i = 0; i < result.length; i++) {
+        $('.xb-show tbody').append(
             `
-            <tr>
-                            <td>${res.data[i]._id}</td>
-                            <td>${res.data[i].name}</td>
-                            <td><img src="${res.data[i].imgUrl}" alt=""></td>
-                            <td><a href="javascript:;" class="delet">删除</a><a href="javascript:;" class="alter" >修改</a></td>
+                        <tr>
+                            <td>${result[i]._id}</td>
+                            <td>${result[i].name}</td>
+                            <td><img src="${result[i].imgUrl}" alt=""></td>
+                            <td><a href="javascript:;" data-id="${result[i]._id}"  class="delet">删除</a><a href="javascript:;" class="alter"  data-toggle="modal" data-target="#myModal1" >修改</a></td>
                         </tr>
             `
-        )  
-            };
-        }else{//获取失败
-            console.log(result.msg);
-            // layer.msg('网络异常，请稍后重试');
+        )
+    };
+}
+//渲染分页
+function showPage(totalPage) {
+    $('#page').html('');
+    $('#page').append(
+        `<li class='prevPage'>
+                            <a href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>`
+    )
+    for (var j = 1; j <= totalPage; j++) {
+        var className='';
+        if (j===page){
+            var className='active';
+        }
+        $('#page').append(
+            `<li class="page ${className}" data-on='${j}'><a href="#">${j}</a></li>`
+        )
+    }
+    $('#page').append(
+        ` <li class='nextPage'><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>`
+    )
+}
+
+// 点击切换页面
+function clickPage(){
+    $("#page").on('click','.page',function(){
+             console.log(page)
+        if (page === parseInt($(this).attr('data-on'))){
+            return;
+        }else{
+            page = parseInt($(this).attr('data-on'));
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+            var Num = $(this).attr('data-on');
+            console.log(Num)
+            getData(Num, pageSize);  
+        }
+       
+    })
+}
+//点击上一页
+function prevPage(){
+    $('#page').on('click', '.prevPage',function(){
+        if(page>1){
+            page--;
+            // clickPage()
+            getData(page, pageSize);
+            console.log(page)
+        }else{
+            console.log(page + '55')
+            return;
         }
         
     })
-};
-// 点击切换页面
-function clickPage(fn){
-    getData(1, 2);
-    for(var i=0;i<$('.page').length;i++){
-        $('.page').eq(i).click(function () {
-            $(this).addClass('active').siblings().removeClass('active');
-           let page=parseInt($(this).find('a').html());
-            $('.xb-show tbody').html('');
-            getData(page, 2);
-            
-        })
-    }
-}
-// 修改
-function alter(){
-    $(".xb-show tbody").delegate(".alter", "click", function (event) {
-            var target = $(event.target);
-             //获取当前的名字、图片
-          let imgUrl = target.parent().prev().find('img').attr('src');
-          let name= target.parent().prev().prev().html();
-       
-        });
-}
-// 删除一条banner数据
-function delet(){
-    $(function () {
-        $(".xb-show tbody").delegate(".delet", "click", function (event) {
-            layer.confirm('是否要删除?', function (index) {
-                var target = $(event.target);
-                target.parent().parent().remove();
-                $.get('/banner/remove', {
-                    name: target.parent().prev().prev().html()
-                }, function (res) {
-                    if (res.code === 0) {
-                        layer.msg('删除成功');
-                    } else {
-                        layer.msg('网络异常，请稍后再试');
-                    }
-                })
-                layer.close(index);
-            });  
-   
-      
-              
-           
-        })
+    $('#page').on('click', '.nextPage', function () {
+        if (page < totalPage) {
+            page++;
+            getData(page,pageSize);
+            console.log(page)
+        } else {
+            console.log(page + '55')
+            return;
+        }
+
     })
 }
+
+
+
+
+
+
+// alter();
+// getData(pageNum, pageSize)第一个第几页，第二个是一页有多少条
+
+
+
+// 删除一条banner数据
+
+// 查询数据
+
+
+//修改banner数据
+
+// function alter() {
+//     $(".xb-show tbody").delegate(".alter", "click", function (event) {
+//         var target = $(event.target);
+//         //获取当前的名字、图片
+//         let imgUrl = target.parent().prev().find('img').attr('src');
+//         let name = target.parent().prev().prev().html();
+//         $('#alterName').val(name);
+//         $('#alterUrlImg').val(imgUrl);
+//         $('#alter').click(function(){
+//             $('#myModal').modal('hide');
+//         })
+//         // addBanner();
+//     });
+// }
--- router/banner.js
+++ router/banner.js
@@ -2,39 +2,99 @@ const express = require('express');
 const router = express.Router();
 const BannerModel = require('../models/banner');
 const async = require('async');
+const fs =require('fs');
+const path = require('path');
+const multer = require('multer');
+// 指定下载的目录
+const upload = multer({
+    dest: 'c:/tmp'
+});
+router.post('/add', upload.single('bannerImg'), (req, res) => {
+    // 1. 操作文件
+    let newFileName = new Date().getTime() + '_' + req.file.originalname;
+    let newFilePath = path.resolve(__dirname, '../public/uploads/banners/', newFileName);
+    // 2. 移动文件
+    try {
+        let data = fs.readFileSync(req.file.path);
+        fs.writeFileSync(newFilePath, data);
+        fs.unlinkSync(req.file.path);
 
-// 添加banner数据
-router.post('/add', function (req, res) {
-    // console.log(req.body.vdName, req.body.imgName);
-    // res.send('成功');
-    var banner = new BannerModel({
-        name: req.body.vdName,
-        imgUrl: req.body.imgName
-    });
-    banner.save(function(err){
-        if (err) {
-            res.json({
-                code: -1,
-                msg: err.message
-            })
-        } else {
-            // 成功
+        // 文件的名字 + banner 图的名字给写入到数据库
+        let banner = new BannerModel({
+            name: req.body.bannerName,
+            imgUrl: 'http://localhost:3000/uploads/banners/' + newFileName
+        });
+
+        banner.save().then(() => {
             res.json({
                 code: 0,
                 msg: 'ok'
             })
-        }
-    })
+        }).catch(error => {
+            res.json({
+                code: -1,
+                msg: error.message
+            })
+        })
+    } catch (error) {
+        res.json({
+            code: -1,
+            msg: error.message
+        })
+    }
+})
+
+// 添加banner数据
+// router.post('/add', function (req, res) {
+//     // res.send('成功');
+//     var banner = new BannerModel({
+//         name: req.body.vdName,
+//         imgUrl: req.body.imgName
+//     });
+//     banner.save(function (err) {
+//         if (err) {
+//             res.json({
+//                 code: -1,
+//                 msg: err.message
+//             })
+//         } else {
+//             // 成功
+//             res.json({
+//                 code: 0,
+//                 msg: 'ok'
+//             })
+//         }
+//     })
+
+// });
+
+// router.post('/add', function (req, res) {
+//     // res.send('成功');
+//     var banner = new BannerModel({
+//         name: req.body.vdName,
+//         imgUrl: req.body.imgName
+//     });
+//     banner.save(function(err){
+//         if (err) {
+//             res.json({
+//                 code: -1,
+//                 msg: err.message
+//             })
+//         } else {
+//             // 成功
+//             res.json({
+//                 code: 0,
+//                 msg: 'ok'
+//             })
+//         }
+//     })
     
-});
+// });
 
 // 查询banner数据
 router.get('/get',function(req,res){
-    let pageNum = parseInt(req.query.pageNum) || 1;   // 当前的页数
+    let pageNum = parseInt(req.query.pageNum) || 2;   // 当前的页数
     let pageSize = parseInt(req.query.pageSize) || 2; // 每页显示的条数
-    // res.send('5')
-    // console.log(req.query.pageNum, req.query.pageSize);
-    // console.log(pageNum, pageSize)
     async.parallel([
         function (cb) {
             BannerModel.find().skip(pageNum * pageSize - pageSize).limit(pageSize).then(data => {
@@ -63,9 +123,8 @@ router.get('/get',function(req,res){
                 res.json({
                     code: 0,
                     msg: 'ok',
-                    // result: result[0].length,
                     data: result[0],
-                    totalPage: Math.ceil(result[0].length / pageSize)
+                    totalPage: Math.ceil(result[1]/ pageSize)
                 });
             }
     })  
@@ -73,9 +132,7 @@ router.get('/get',function(req,res){
 // 查找banner的_id数据删除
 router.get('/remove',function(req,res){
     console.log(req.query.name);
-    
-    // console.log(55)
-    BannerModel.find({ name: req.query.name}).remove(function(err){
+    BannerModel.find({ _id: req.query.name}).remove(function(err){
         if(err){
             console.log("删除失败");
             res.json({
@@ -84,6 +141,7 @@ router.get('/remove',function(req,res){
             });
         }else{
             console.log("删除成功");
+            //删除public下面的图片
             res.json({
                 code: 0,
                 msg: 'ok',
